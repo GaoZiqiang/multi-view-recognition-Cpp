4a46fdba851d4d770977e3b23eddac078952e8bb
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-01-14 19:25:10 +0800

    model

--- model/ModelManager.cpp
+++ model/ModelManager.cpp
@@ -1,5 +1,42 @@
 //
 // Created by gaoziqiang on 2021/1/14.
 //
+#include "../util/DataManager.h"//注意路径
 
-#include "ModelManager.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <torch/torch.h>
+#include <torch/script.h>
+
+using namespace std;
+using namespace cv;
+
+torch::jit::script::Module LoadResnet50(){
+    cout << "func:LoadResnet50" << endl;
+
+    //使用torch::jit::script加载resnet50
+    torch::jit::script::Module model = torch::jit::load("../model/modified_resnet.pt");
+
+    cout << "resnet50 loaded successfully" << endl;
+
+    return model;
+}
+
+torch::Tensor FeatureExtracting(string path){
+    cout << "func:FeatureExtracting" << endl;
+
+    LoadResnet50();
+    torch::jit::script::Module model = LoadResnet50();
+
+    torch::Tensor img_tensors = ImgToTensor(path);
+
+
+    torch::Tensor feature = model.forward({img_tensors}).toTensor();
+
+
+    return feature;
+}
--- model/ModelManager.h
+++ model/ModelManager.h
@@ -1,14 +1,14 @@
 //
 // Created by gaoziqiang on 2021/1/14.
 //
+#include <torch/torch.h>
+#include <torch/script.h>
+#include <string>
 
-#ifndef MULTIVIEWRECOGCPP_MODELMANAGER_H
-#define MULTIVIEWRECOGCPP_MODELMANAGER_H
+using namespace std;
+
+#pragma once//防止多次定义
 
 
-class ModelManager {
-
-};
-
-
-#endif //MULTIVIEWRECOGCPP_MODELMANAGER_H
+extern torch::jit::script::Module LoadResnet50();
+extern torch::Tensor FeatureExtracting(string path);
